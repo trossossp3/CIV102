f27c96f49d1bb8f4c392bf6dc0b467f780dad73f
trossossp3 <21trossos@gmail.com>
2021-12-01 20:26:29 -0500

    final

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def printBMD():
     zeroliney = [0, 0]
     zerolinex = [0, L]
     plt.plot(zerolinex, zeroliney)
-    # plt.plot(*zip(*sorted(buildBMD(xP, P))))
+
     plt.gca().invert_yaxis()
     plt.plot(bmd)
     plt.show()    
@@ -159,29 +159,16 @@ def ybar(height, widthtop, widthbottom, topthickness, bottomthickness, rightthic
     """
     if(shape == 0):#shape = 0 during test bridge    
             area1 = widthtop * topthickness #top part of bridge
-            area2 = 5 * tabThickness #tabs 10 is tab width
+            area2 = 10 * (tabThickness) #tabs 10 is tab width
             area3 = leftthickness * (height - topthickness)
             area4 = (widthbottom - 2*leftthickness) * bottomthickness
             d1 = (height-topthickness/2)
-            d2 = (height - topthickness - topthickness/2)
-            d3 = (height-2*topthickness)/2
+            d2 = (height - topthickness - tabThickness/2)
+            d3 = (height-topthickness)/2
             d4 = bottomthickness/2
             y_bar = area1*d1 + 2*(area2*d2) + 2*(area3*d3) + area4*(d4)     
             y_bar = y_bar / (area1+2*area2+2*area3+area4)
             return y_bar
-
-    elif(shape ==1):
-        area1 = widthtop * topthickness #top part of bridge
-        area2 = 10 * bottomthickness #tabs 10 is tab width
-        area3 = leftthickness * (height - topthickness - bottomthickness)
-        area4 = (widthbottom - 2*leftthickness) * bottomthickness
-        d1 = (height-topthickness/2)
-        d2 = (height - topthickness - topthickness/2)
-        d3 = (height-2*topthickness)/2
-        d4 = bottomthickness/2
-        y_bar = area1*d1 + 2*(area2*d2) + 2*(area3*d3) + area4*(d4)     
-        y_bar = y_bar / (area1+2*area2+2*area3+area4)
-        return y_bar
 
 def I0(b, h):
     """
@@ -214,12 +201,12 @@ def second_moment_of_area(height, widthtop, widthbottom, topthickness, bottomthi
     centroid = ybar(height, widthtop, widthbottom, topthickness, bottomthickness, rightthickness, leftthickness, shape, tabThickness)
     if shape == 0:
         area1 = widthtop * topthickness #top part of bridge
-        area2 = 5 * topthickness #tabs 10 is tab width
-        area3 = leftthickness * (height - 2*topthickness)
+        area2 = 10 * (tabThickness) #tabs 10 is tab width
+        area3 = leftthickness * (height - topthickness)
         area4 = (widthbottom - 2*leftthickness) * bottomthickness
         d1L = (height-topthickness/2)
-        d2L = (height - topthickness - topthickness/2)
-        d3L = (height-2*topthickness)/2
+        d2L = (height - topthickness - tabThickness/2)
+        d3L = (height-topthickness)/2
         d4L = bottomthickness/2
         d1C = d1L - centroid
         d2C = d2L - centroid
@@ -227,10 +214,12 @@ def second_moment_of_area(height, widthtop, widthbottom, topthickness, bottomthi
         d4C = d4L - centroid
 
         term1 = I0(widthtop, topthickness) + (area1 * (d1C)**2) #top
-        term2 = I0(5, tabThickness) + area2 * (d2C)**2 #tabs
-        term3 = I0(leftthickness, height - 2*topthickness) + (area3 * (d3C)**2)
+        term2 = I0(10, tabThickness) + area2 * (d2C)**2 #tabs
+        term3 = I0(leftthickness, height - topthickness) + (area3 * (d3C)**2)
         term4 = I0(widthbottom - 2*leftthickness, bottomthickness) + (area4 * (d4C)**2)
         second_moment_of_area = term1 + 2* term2 + 2*term3 + term4
+
+
     return second_moment_of_area
 
 
@@ -291,7 +280,7 @@ def get_properties():
     bottomThickness = np.concatenate(([1.27]*520, [1.27*2]*50, [1.27]*570, [1.27*2]*(L-520-50-570)), axis = 0) #double bottom thickness between 520 - 570 and 1140 - the end
     rightThickness = np.concatenate(([1.27]*540, [1.27 * 2]*10, [1.27]*(L-540-10)), axis = 0) #double thickness between 540-550
     leftThickness = np.concatenate(([1.27]*540, [1.27 * 2]*10, [1.27]*(L-540-10)), axis = 0) #double thickness between 540-550
-   
+        
     shape = [0] * L #similar crosssection through the entire length
     tabThickness = [1.27]*L
     return  height, widthTop, widthBottom, topThickness, bottomThickness, rightThickness, leftThickness, shape, tabThickness
@@ -310,7 +299,7 @@ def y_top(y_bar, height):
 def geometric_properties():
     """
     functions to generate lists of geometric properties
-    each properties is length L with each entry being the value at each point on the bridge
+    each properties is a list, length L with each entry being the value of that property at each point on the bridge
 
     returns
         I(list): second moment of area at each point on the bridge
@@ -334,6 +323,13 @@ def geometric_properties():
     return I, y_bar, Q, yTop
 
 def V_fail(Tau):
+    """
+    functiont to calculate the values of sheer needed to cause shear failure
+        parameters:
+           Tau(int): shear strength
+        returns:
+            V_fail_values(list): the values of sheer that would cause a shear failure
+    """
   
     I = geometric_properties()[0]
     Q = geometric_properties()[2]
@@ -369,6 +365,13 @@ def V_fail(Tau):
     return V_fail_values
 
 def V_buck(max_diaphragm_distance):
+    """
+    functiont to calculate the values of sheer needed to cause shear buckling
+        parameters:
+            max_diaphragm_distance(int): max distance between any two diaphgram
+        returns:
+            V_buck_values(list): the values of sheer that would cause a failure due to shear buckling
+    """
 
     height = get_properties()[0]
     widthtop = get_properties()[1]
@@ -407,29 +410,18 @@ def V_buck(max_diaphragm_distance):
     return V_buck_values        
 
 def M_failMatT(sigT):
-    global L
-    """
-    calculate moment the moment the causes tension failure at each x
-    returns array of the moment required to break bridge at each location
     
-    sectional_properties array with I, Q, yBar, Ybot, Ytop
-        each indivisual properties should be an array with the value at each location along the bridge
-    sigT = sigma tentsion
-    BMD 1-d array of the bending moments at each mm of the bridge
+    """
+    calculate  the moment that causes tension failure at each x
+        returns:
+            moment_fail(list): list of momement that would cause the bridge to break at each location along the bridge
 
     """
     # change with right indices
-    height = get_properties()[0]
-    widthtop = get_properties()[1]
-    widthbottom = get_properties()[2]
-    topthickness = get_properties()[3]
-    bottomthickness = get_properties()[4]
-    rightthickness = get_properties()[5]
-    leftthickness = get_properties()[6]
-
+   
     I = geometric_properties()[0]
     y_bar = geometric_properties()[1]
-    Q = geometric_properties()[2]
+
     y_top = geometric_properties()[3]
     
     # ##########################
@@ -449,12 +441,10 @@ def M_failMatT(sigT):
 def M_failMatC(sigC):
     global L
     """
-    calculate moment the moment the causes compression failure at each x
+    calculate the moment the causes compression failure at each x
         returns:
-        moment_fail(list): array of the moment required to break bridge at each location
+            moment_fail(list): array of the moment required to break bridge at each location
    
-
-
     """
     # change with right indices
     I = geometric_properties()[0]
@@ -477,6 +467,11 @@ def MFailBuck(t):
     
     """
     calculated moment needed to break bridge due to buckling at every location along the bridge
+    checks wich form of buckling is most prevelant:
+        mid flange buckling
+        side flange buckling
+        web compression buckling
+    it will then add the corresponding moment to list moments
 
     parameters:
         t(float): thickness 
@@ -523,23 +518,23 @@ def MFailBuck(t):
                 n2 = sigma3*I[i] / y_bar[i] 
                 sigmas3.append(min(n1,n2))
             elif bmd[i]<0:
-                sigma1 = case1(i, topthickness[i], 80)
+                sigma1 = case1(topthickness[i], 80)
                 n1 =  -1* sigma1*I[i] / y_top[i]
                 n2 = -1* sigma1*I[i] / y_bar[i] 
                 sigmas1.append(min(n1,n2))
                 # sigma1 = case1(i, t, 80)
-                sigma2 = case2(i, t,10)
+                sigma2 = case2(t,10)
                 
                 n1 =  -1*sigma2*I[i] / y_top[i]
                 n2 = -1*sigma2*I[i] / y_bar[i] 
                 sigmas2.append(min(n1,n2))
                 
-                sigma3 = case3(i, t, 32.02)
+                sigma3 = case3(t, 32.02)
                 n1 =  -1*sigma3*I[i] / y_top[i]
                 n2 = -1*sigma3*I[i] / y_bar[i] 
                 sigmas3.append(min(n1,n2))
             else:
-                sigma1 = case1(i, topthickness[i], 80)
+                sigma1 = case1(topthickness[i], 80)
                 n1 =  -1* sigma1*I[i] / y_top[i]
                 n2 = -1* sigma1*I[i] / y_bar[i] 
                 sigmas1.append(0)
@@ -550,7 +545,7 @@ def MFailBuck(t):
                 n2 = -1*sigma2*I[i] / y_bar[i] 
                 sigmas2.append(0)
                 
-                sigma3 = case3(i, t, 32.02)
+                sigma3 = case3(t, 32.02)
                 n1 =  -1*sigma3*I[i] / y_top[i]
                 n2 = -1*sigma3*I[i] / y_bar[i] 
                 sigmas3.append(0)
@@ -565,18 +560,18 @@ def MFailBuck(t):
         
     return(moments)    
 
-def case1(i,t, b):    
+def case1(t, b):    
     n1 = (4*math.pi**2 * E)
     n2 = (12*(1-MU**2))
     n3 = (t/b)**2
     return n1/n2 * n3
 
-def case2(i, t, b):
+def case2(t, b):
     
     sigma = (0.425*math.pi**2 * E)/(12*(1-MU**2))*(t/b)**2
     return sigma
 
-def case3(i, t, b):
+def case3(t, b):
    
     sigma = (6*math.pi**2 * E)/(12*(1-MU**2))*(t/b)**2
     return sigma
@@ -599,10 +594,10 @@ def testFail():
     p = 600
    
     while not broken1 and not broken2 and not broken3 and not broken4 and not broken5:
-        buildSFD(550, p)
+        buildSFD(565, p)
         # printSFD()
 
-        arr = buildSFD(1250,p)
+        arr = buildSFD(1265,p)
         # print(arr[676])
         with open('your_file.txt', 'w') as f:
             for item in arr:
@@ -640,6 +635,8 @@ def testFail():
 def testFailTrain():
     """
     function to test failure load when the train is in the middle of the supports
+    incrimentally adds 1 to P until the bridge fails
+    functions prints what caused failure
 
     """
     broken1 = False
@@ -647,10 +644,10 @@ def testFailTrain():
     broken3 = False
     broken4 = False
     broken5 = False
-    p = 42
+    p = 30
     # while not broken1 or not broken2 or not broken3:
     while not broken1 and not broken2 and not broken3 and not broken4 and not broken5:
-        #for values at the right
+        #for values of train at the right
         # buildSFD(372, p)
         # buildSFD(548,p)
         # buildSFD(712, p)
@@ -699,33 +696,53 @@ def testFailTrain():
                 
         p+=1
 
-def deflection(bmd):
+def deflection():
     
     """
-    assuming a max positive moment and the bridge is deflecting down
+    function to return midspan deflection of the bridge based on point loads of 200N
 
-    for midspsan delfection:
-        use similar triangles with the tangent drawn at the support on the left
-        delta_C_a = tangential deviation on the right support to tangent at a
+        function uses the areas of the various triangles formed on the BMD to calculate the midspan deflection
+        uses similar triangles with tangential deviation from the min moment to the tangenet draw at the begining of the curvutre diagra,
+    
+    returns midspan deflection
 
-    curvature max becomes the height of the similar trianggles
-     delta_c_a / L = (delta_mid_a + deflection)/L/2
     """            
-    I = geometric_properties()[0]
-    y_bar = geometric_properties()[1]
-    Q = geometric_properties()[2]
-    y_top = geometric_properties()[3]
+    p=200
+    buildSFD(550, p)
 
+    arr = buildSFD(1250,p)
+    buildBMD()
+    I = geometric_properties()[0]
+ 
+
+   
+    mid_point = int((550+510)/2)
 
     curvatures = []
-    for i in range(bmd):
+    buildBMD()
+    for i in range(len(bmd)):
         curvatures.append(bmd[i]/(E*I[i]))
-    max_curv = max(curvatures)
-    curvature_midpoint_scale = 1 - (curvatures.index(max)/L-1/2) #not sure how this will work for if it is curved before midpoint
-    delta_c_a = (1/2*(L/2)*max_curv * (L/2 + (1/3)*(L/2)))+ (1/2*L/2 * max_curv * (L/2*(2/3))) #sum(Ai*di) where di is distance from c to centroid of area
-    delta_m_a = (1/2 *(L/2) *max_curv*curvature_midpoint_scale) * (1/3*(L/2))
-    deflection = delta_c_a/L * (L/2) -delta_m_a
-    return deflection
+    
+    print("10")
+    distance_from_max_moment_to_0_moment = 237
+    distance_from_0_moment_to_min_moment = 272
+    distance_from_min_moment_to_end = 190
+    distance_to_max = 550+support_x
+    distance_to_mid = 530+support_x_right
+    curv_mid = curvatures[mid_point+ support_x]
+    curv_max = curvatures[550 + support_x]
+    curv_min = curvatures[550+distance_from_max_moment_to_0_moment+distance_from_0_moment_to_min_moment+support_x]
+
+    delta_c_a = 1/2*(curv_max)*distance_to_max*(distance_from_max_moment_to_0_moment + distance_from_0_moment_to_min_moment+1/3*(distance_to_max))
+    delta_c_a += 1/2*(curv_max * distance_from_max_moment_to_0_moment)*(distance_from_0_moment_to_min_moment + 2/3*distance_from_max_moment_to_0_moment)
+    delta_c_a+= 1/2*(curv_min)*distance_from_0_moment_to_min_moment*1/3*distance_from_0_moment_to_min_moment
+
+    delta_mid_a = 1/2 *curv_min * distance_to_mid * 1/3*distance_to_mid
+
+    delta_mid = 1/2 *delta_c_a - delta_mid_a
+
+    return delta_mid
+    
 
 def graphs():
     """
@@ -736,7 +753,7 @@ def graphs():
     arr = buildSFD(1250,p)
     printSFD()
     buildBMD()
-    # printBMD()        
+    printBMD()        
     m_buckle = MFailBuck(1.27)
     m_tension = M_failMatT(30)  
     m_compression = M_failMatC(6)
@@ -832,6 +849,85 @@ def shearBuck(buck):
     plt.show()
 
 
+def FOStrain():
+    """
+    function to calculate FOS between the shear forces and internal moments
+
+    the FOS at each point of the bridge is calculated by dividing the fail causing failure by the actual expierenced values
+    the minimum of these values is then used as the FOS
+
+        returns:
+            min(fos_m)(float): FOS for the moments
+            min(fos_v)(float): FOS for the shear forces
+    """
+    global sfd
+     # for values at center
+    p=400/6
+    buildSFD(117, p)
+    buildSFD(293,p)
+    buildSFD(457, p)
+    buildSFD(633,p)
+    buildSFD(797, p)
+    arr =buildSFD(973,p)
+    buildBMD()
+    m_fail_comp = M_failMatC(6)
+    shear_fail = V_fail(4)
+    m_fail_buck = MFailBuck(1.27)
+    fos_m = []
+
+    fos_v = []
+    for i in range(len(bmd)):
+        if(bmd[i] != 0):
+            fos_m.append(m_fail_comp[i] / bmd[i])
+            fos_m.append(m_fail_buck[i]/bmd[i])
+        if(arr[i] != 0):
+            fos_v.append(shear_fail[i]/abs(arr[i]))
+    return min(fos_m), min(fos_v)
+
+
+def glue_tabs(Tau):
+    """
+    returns the shear needed to break the glue
+
+        parameters:
+            Tau(float): the shear strength of the glue
+        returns:
+            glue_tab_values(list): the shear needed to break the glue at anypoint along the bridge
+    """
+    I = geometric_properties()[0]
+    Q = geometric_properties()[2]
+    centroid = geometric_properties()[1]
+    bottomthickness = get_properties()[4]
+    height = get_properties()[0]
+    topthickness = get_properties()[3]
+    widthbottom = get_properties()[2]
+    widthtop = get_properties()[1]
+    rightthickness = get_properties()[5]
+    leftthickness = get_properties()[6]
+    glue_tabs_values = []
+
+    for i in range(L):
+        if 0 <= centroid[i] < bottomthickness[i]:
+            centroidlocation = "bottom"
+        elif bottomthickness[i] <= centroid[i] < height[i] - topthickness[i]:
+            centroidlocation = "middle"
+        elif height[i] - topthickness[i] <= centroid[i] < height[i]:
+            centroidlocation = "top"
+
+        if centroidlocation == "bottom":
+            b = widthbottom[i]
+        elif centroidlocation == "middle":
+            b = rightthickness[i] + leftthickness[i]
+        elif centroidlocation == "top":
+            b = widthtop[i]
+
+        glue_tab_force = (Tau * I[i] * b)/Q[i]
+
+        glue_tabs_values.append(glue_tab_force)
+
+    return glue_tabs_values
+
+
 
 if __name__ == "__main__":
     # buildSFD(100,50)
@@ -841,5 +937,7 @@ if __name__ == "__main__":
     # init()
     # testFail()
     # testFailTrain()
-    graphs()
+    # graphs()
+    # print(FOStrain())
+    print(deflection())
 
